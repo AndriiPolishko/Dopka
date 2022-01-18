7e7301902b1c16e07e5343ca0bf9da7c2d7c4127
Dima <omeluan.dima@gmail.com>
2022-01-18 19:50:07 +0200

    Create input field and button

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,27 @@
 <script>
+    let defaultValue = "";
+    let url = defaultValue;
+
+    const go = () => {
+        console.log(url);
+        resetDefaultValues();
+    }
+
+    const resetDefaultValues = () => {
+        url = defaultValue;
+    }
 
 </script>
 
 <main>
-
+    <label>
+        Input HTTP URL <input bind:value={url} class="input-field">
+    </label>
+    <button on:click={go}>Go</button>
 </main>
 
 <style>
-
+.input-field {
+    outline: none;
+}
 </style>
