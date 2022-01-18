e4502b9c70224a0eeccd513f813d2ccb7f8fca10
Dima <omeluan.dima@gmail.com>
2022-01-18 20:07:14 +0200

    Add styles for button and input field + some refactoring

--- src/App.svelte
+++ src/App.svelte
@@ -14,14 +14,37 @@
 </script>
 
 <main>
-    <label>
-        Input HTTP URL <input bind:value={url} class="input-field">
-    </label>
-    <button on:click={go}>Go</button>
+    <div class="main-div text-component">
+        <h1>HTTP Web Proxy</h1>
+        <input placeholder="Enter url" bind:value={url} class="input-field text-component">
+        <button on:click={go} class="button-style text-component">Go</button>
+    </div>
 </main>
 
 <style>
+
+.main-div {
+    text-align: center;
+    padding-top: 10%;
+}
+
 .input-field {
     outline: none;
+    border-radius: 2px;
+    height: 30px;
+    width: 500px;
+    text-align: center;
 }
+
+.text-component {
+    font-size: 25px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.button-style {
+    border-radius: 2px;
+    height: 38px;
+    width: 100px;
+}
+
 </style>
