ae52240aa12a6e07b5141512438aea4bcbbdb978
Dima <omeluan.dima@gmail.com>
2022-01-19 21:11:27 +0200

    Fix githuv actions

--- api/backend.js
+++ api/backend.js
@@ -1,11 +1,3 @@
-async function FetchHtml(url) {
-  let response = await fetch(url);
-  console.log("HTML: " + response.text());
-  return await response.text();
-}
-
 module.exports = async (req, res) => {
-  // res.json({ a: req.body.url });
-  let url = req.body.url;
-  return FetchHtml(url);
+  res.json({ a: req.body.url });
 };
