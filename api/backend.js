8cf0bcd9493c5be38e59499089b3a40e2c84a17f
Andrii <ramp8a799@gmail.com>
2022-01-19 19:08:15 +0200

    server and POST request is working now

--- api/backend.js
+++ api/backend.js
@@ -1,19 +1,3 @@
-const http = require("http");
-const httpProxy = require("http-proxy");
-
-const proxy = httpProxy.createProxyServer({
-  changeOrigin: true,
-  autoRewrite: true,
-  hostRewrite: true,
-  followRedirects: true,
-});
-
-const server = http.createServer(function (req, res) {
-  // load from ENVs
-  const origin = process.env.ORIGIN;
-
-  proxy.on("proxyRes", function (proxyRes, req, res) {
-    proxyRes.headers["x-proxy"] = "http-proxy";
-  });
-  proxy.web(req, res, { target: `${origin}` });
-});
+module.exports = async (req, res) => {
+  res.json({ a: req.body.url });
+};
--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,20 @@
 <script>
-   let defaultValue = "";
-   let url = defaultValue;
-   let urlObj = {};
-   const go = async (e) => {
-       e.preventDefault();
-       urlObj.url = url;
-       console.log(url);
-       let responce = await fetch("./api/backend.js",{
-           method:"POST",
-           body: JSON.stringify(urlObj)
-       });
-       resetDefaultValues();
+    let defaultValue = "";
+    let url = defaultValue;
+    let urlObj = {};
+    let API_URL = "./api/backend";
+    let POST_result = "";
+    const go = async (e) => {
+        e.preventDefault();
+        urlObj.url = url;
+        console.log(url);
+        await fetch(API_URL, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(urlObj)
+        });
+
+        resetDefaultValues();
     };
 
 
@@ -21,39 +25,39 @@
 </script>
 
 <main>
-    <div class="main-div text-component">
+    <div class="main-div text-comclassNamet">
         <h1>HTTP Web Proxy</h1>
         <form action="./api/backend.js">
-            <input placeholder="Enter url" bind:value={url} class="input-field text-component">
-            <button on:click={go} class="button-style text-component">Go</button>
+            <input placeholder="Enter url" bind:value={url} class="input-field text-classNamenent">
+            <button on:click={go} class="button-style textclassNameonent">Go</button>
         </form>
     </div>
 </main>
 
 <style>
 
-.main-div {
-    text-align: center;
-    padding-top: 10%;
-}
+    .main-div {
+        text-align: center;
+        padding-top: 10%;
+    }
 
-.input-field {
-    outline: none;
-    border-radius: 2px;
-    height: 30px;
-    width: 500px;
-    text-align: center;
-}
+    .input-field {
+        outline: none;
+        border-radius: 2px;
+        height: 30px;
+        width: 500px;
+        text-align: center;
+    }
 
-.text-component {
-    font-size: 25px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
+    .text-component {
+        font-size: 25px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
 
-.button-style {
-    border-radius: 2px;
-    height: 38px;
-    width: 100px;
-}
+    .button-style {
+        border-radius: 2px;
+        height: 38px;
+        width: 100px;
+    }
 
 </style>
