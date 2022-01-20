7fc081a9883ccbe641aae5302136bda62ee932f2
Dima <omeluan.dima@gmail.com>
2022-01-20 07:43:06 +0200

    Read html from url added

--- api/backend.js
+++ api/backend.js
@@ -1,3 +1,48 @@
+let http = require("http");
+let httpProxy = require("http-proxy");
+let proxy = httpProxy.createProxyServer({});
+const https = require("https");
+
+function readURL(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        // получаем статус ответа сервера посредством деструктуризации объекта
+        const { statusCode } = res;
+
+        let error;
+        if (statusCode !== 200) {
+          error = new Error(`Ошибка запроса. Код ответа: ${statusCode}`);
+        }
+
+        if (error) {
+          reject(error);
+          res.resume();
+          return;
+        }
+
+        res.setEncoding("utf8");
+
+        let rawData = "";
+        res.on("data", (chunk) => (rawData += chunk));
+
+        res.on("end", () => resolve(rawData));
+      })
+      .on("error", (e) => reject(e));
+  });
+}
+
 module.exports = async (req, res) => {
   res.json({ a: req.body.url });
+
+  let html;
+
+  await readURL(req.body.url)
+    .then((data) => {
+      html = data;
+      console.log("Html1 - " + html);
+    })
+    .catch((err) => console.log(err.message));
+
+  console.log("Html2 - " + html);
 };
