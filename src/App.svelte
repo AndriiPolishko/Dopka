3c8542ea02b4fb6e3b602c224080275adf17a425
Dima <omeluan.dima@gmail.com>
2022-01-21 20:53:32 +0200

    changes in front side - minimal ones

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,55 @@
+<style>
+.iframe-style {
+  width: 100%;
+  height: 800px;
+}
+.main-div {
+  text-align: center;
+}
+.input-field {
+  outline: none;
+  border-radius: 2px;
+  height: 30px;
+  width: 500px;
+  text-align: center;
+}
+.button-style {
+  border-radius: 2px;
+  height: 38px;
+  width: 100px;
+}
+</style>
+
 <script>
-   let defaultValue = "";
-   let url = defaultValue;
-   let urlObj = {};
-   let deployUrl = '';
+let defaultValue = "";
+let url = defaultValue;
+let urlObj = {};
+let deployUrl = "https://backend-dopka.vercel.app/";
 
-   const go = () => {
-       document.getElementById('myIframe').setAttribute("src", changeUrl(url));
-    };
+const go = () => {
+  document.getElementById("myIframe").setAttribute("src", changeUrl(url));
+  resetDefaultValues();
+};
 
-    const resetDefaultValues = () => {
-        url = defaultValue;
-    };
+const resetDefaultValues = () => {
+  url = defaultValue;
+};
 
-    const changeUrl = (url) => {
-        return deployUrl + url;
-    };
+const changeUrl = (url) => {
+  return deployUrl + url;
+};
 </script>
 
 <main>
-    <div class="main-div text-comclassNamet">
-        <h1>HTTP Web Proxy</h1>
-        <input placeholder="Enter url" bind:value={url} class="input-field text-classNamenent">
-        <button on:click={go} class="button-style textclassNameonent">Go</button>
-    </div>
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <iframe id="myIframe" src="" frameborder="0" class="iframe-style"></iframe>
+  <div class="main-div text-comclassNamet">
+    <h1>HTTP Web Proxy</h1>
+    <input
+      placeholder="Enter url"
+      bind:value="{url}"
+      class="input-field text-classNamenent"
+    />
+    <button on:click="{go}" class="button-style textclassNameonent">Go</button>
+  </div>
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <iframe id="myIframe" src="" frameborder="0" class="iframe-style"></iframe>
 </main>
