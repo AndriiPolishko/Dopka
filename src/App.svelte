7556afe922a56b36bf62f738f1259bd6fd2a88ed
Dima <omeluan.dima@gmail.com>
2022-01-21 22:15:27 +0200

    Fix something

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,60 @@
-<style>
-.iframe-style {
-  width: 100%;
-  height: 800px;
-}
-.main-div {
-  text-align: center;
-}
-.input-field {
-  outline: none;
-  border-radius: 2px;
-  height: 30px;
-  width: 500px;
-  text-align: center;
-}
-.button-style {
-  border-radius: 2px;
-  height: 38px;
-  width: 100px;
-}
-</style>
-
 <script>
-let defaultValue = "";
-let url = defaultValue;
-let urlObj = {};
-let deployUrl = "https://backend-dopka.vercel.app/";
+   let defaultValue = "";
+   let url = defaultValue;
+   let urlObj = {};
+   let deployUrl = 'https://backend-dopka-git-dev-andriipolishko.vercel.app/';
 
-const go = () => {
-  document.getElementById("myIframe").setAttribute("src", changeUrl(url));
-  resetDefaultValues();
-};
+   const go = async (e) => {
+        e.preventDefault();
+        urlObj.ourUrl = url;
 
-const resetDefaultValues = () => {
-  url = defaultValue;
-};
+       document.getElementById('myIframe').setAttribute("src", deployUrl);
+       resetDefaultValues();
+    };
 
-const changeUrl = (url) => {
-  return deployUrl + url;
-};
+    const resetDefaultValues = () => {
+        url = defaultValue;
+    };
+
+    const changeUrl = (url) => {
+        return deployUrl + changeUrl;
+    }
+
 </script>
 
 <main>
-  <div class="main-div text-comclassNamet">
-    <h1>HTTP Web Proxy</h1>
-    <input
-      placeholder="Enter url"
-      bind:value="{url}"
-      class="input-field text-classNamenent"
-    />
-    <button on:click="{go}" class="button-style textclassNameonent">Go</button>
-  </div>
-  <!-- svelte-ignore a11y-missing-attribute -->
-  <iframe id="myIframe" src="" frameborder="0" class="iframe-style"></iframe>
+    <div class="main-div text-comclassNamet">
+        <h1>HTTP Web Proxy</h1>
+        <input placeholder="Enter url" bind:value={url} class="input-field text-classNamenent">
+        <button on:click={go} class="button-style textclassNameonent">Go</button>
+    </div>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <iframe id="myIframe" src="" frameborder="0" class="iframe-style"></iframe>
 </main>
+
+<style>
+
+    .iframe-style {
+        width: 100%;
+        height: 800px;
+    }
+
+    .main-div {
+        text-align: center;
+    }
+
+    .input-field {
+        outline: none;
+        border-radius: 2px;
+        height: 30px;
+        width: 500px;
+        text-align: center;
+    }
+
+    .button-style {
+        border-radius: 2px;
+        height: 38px;
+        width: 100px;
+    }
+
+</style>
